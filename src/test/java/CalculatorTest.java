df1751755e9b84299b9107d957e174c97eb57724
NightmareNight-em <nigamharshit1712@gmail.com>
2022-04-14 15:01:19 +0530

    Changing Calculator.java

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,35 +1,37 @@
-import static org.junit.Assert.*;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 public class CalculatorTest {
     private static final double DELTA = 1e-15;
     Calculator calculator = new Calculator();
     @Test
     public void testsqrt(){
-        assertEquals("Square root of positive integer", 10, calculator.sqrt(100),DELTA);
-        assertEquals("Square root of positive integer", 10, calculator.sqrt(100),DELTA); //for decimal
-        assertEquals("Square root of negative integer", -1, calculator.sqrt(-4), DELTA);
+        assertEquals("Square root of positive integer", 10, calculator.squareRoot(100),DELTA);
+        assertEquals("Square root of positive integer", 10, calculator.squareRoot(100),DELTA); //for decimal
+        assertEquals("Square root of negative integer", -1, calculator.squareRoot(-4), DELTA);
     }
     @Test
     public void testlog(){
-        assertEquals("Log of positive number", 2.302585092994046, calculator.log(10), DELTA);
-        assertEquals("Log of negative number", -1, calculator.log(-9), DELTA);
-        assertEquals("Log of 0", -1, calculator.log(0), DELTA);
+        assertEquals("Log of positive number", 2.302585092994046, calculator.logarithm(10), DELTA);
+        assertEquals("Log of negative number", -1, calculator.logarithm(-9), DELTA);
+        assertEquals("Log of 0", -1, calculator.logarithm(0), DELTA);
+        assertEquals("Log of negative number", -1, calculator.logarithm(-9), DELTA);
     }
     @Test
     public void testpow(){
-        assertEquals("Power of a and b where a and b are positive", 81, calculator.pow(3,4), DELTA);
-        assertEquals("Power of a and b where a and b are negetive", 0.25, calculator.pow(-2,-2), DELTA);
-        assertEquals("Power of a and b where a is negative and b is positive", 81, calculator.pow(-3,4), DELTA);
-        assertEquals("Power of a and b where a is positive and b is negetive", 0.25, calculator.pow(2,-2), DELTA);
-        assertEquals("Log of negative number", -1, calculator.log(-9), DELTA);
+        assertEquals("Power of a and b where a and b are positive", 81, calculator.power(3,4), DELTA);
+        assertEquals("Power of a and b where a and b are negetive", 0.25, calculator.power(-2,-2), DELTA);
+        assertEquals("Power of a and b where a is negative and b is positive", 81, calculator.power(-3,4), DELTA);
+        assertEquals("Power of a and b where a is positive and b is negetive", 0.25, calculator.power(2,-2), DELTA);
+
     }
     @Test
     public void testfact(){
-        assertEquals("Fatorial of a where a is positive", 120, calculator.fact(5), DELTA);
-        assertEquals("Fatorial of a where a is positive", 1, calculator.fact(0), DELTA);
-        assertEquals("Fatorial of a where a is negative", -1, calculator.fact(-23), DELTA);
-        assertEquals("Fatorial of a where a is positive", 1, calculator.fact(1), DELTA);
-        assertEquals("Fatorial of a where a is positive", 39916800, calculator.fact(11), DELTA);
+        assertEquals("Fatorial of a where a is positive", 120, calculator.factorial(5), DELTA);
+        assertEquals("Fatorial of a where a is positive", 1, calculator.factorial(0), DELTA);
+        assertEquals("Fatorial of a where a is negative", -1, calculator.factorial(-23), DELTA);
+        assertEquals("Fatorial of a where a is positive", 1, calculator.factorial(1), DELTA);
+        assertEquals("Fatorial of a where a is positive", 39916800, calculator.factorial(11), DELTA);
        
     }
 }
